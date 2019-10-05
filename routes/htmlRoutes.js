59d8d2effdf3e06450d43c9b6f8b78925f08042d
Jackson Oppenheim <jacksonopp@gmail.com>
2019-10-05 12:21:15 -0500

    got each article displayed with basic handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,11 +4,17 @@ const Article = require("../models/article");
 module.exports = function (app) {
     app.get("/", function (req, res) {
         Article.find({}, (err, data) => {
+            if (err) throw err;
             console.log(data);
             res.render("index", { article: data });
         })
     })
     app.get("/:id", function (req, res) {
-
+        const id = req.params.id;
+        Article.findById(id, (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            res.render("article", data);
+        })
     })
 }
